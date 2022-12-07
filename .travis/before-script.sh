042597a870c1c3af243abcb31b14668fbcb42055
Tim Gee <writetotimgee@gmail.com>
2022-12-07 19:41:13 +0000

    Update GPG key expiry

--- .travis/before-script.sh
+++ .travis/before-script.sh
@@ -1,6 +1,6 @@
 
 #!/usr/bin/env bash
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_5cf55422e97e_key -iv $encrypted_5cf55422e97e_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_81c98acad902_key -iv $encrypted_81c98acad902_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
     gpg --fast-import .travis/codesigning.asc
 fi
